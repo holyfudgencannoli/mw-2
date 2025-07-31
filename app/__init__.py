b5b8d9f7d32c7031ec40673fc98db1fd831975f0
holyfudgencannoli <moshimoshi8246@gmail.com>
2025-07-31 18:32:03 -0400

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,14 +1,11 @@
 from flask import Flask
-from .static.static import app_bp
-
-
-
+from flask_cors import CORS
 
 def create_app():
-    app = Flask(__name__, static_folder='../frontend/dist', static_url_path='')
+    app = Flask(__name__)
+    CORS(app)
 
-    app.register_blueprint(app_bp)
-
-    # Base.metadata.create_all(bind=engine)
+    from .routes import main
+    app.register_blueprint(main)
 
     return app
